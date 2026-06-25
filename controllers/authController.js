7da36aab155dadfed86911359ddbec9eef890ee9
Sobu shah <sobukumar568@gmail.com>
2026-06-25 18:33:56 +0530

    created api to register the user and add token based authentication

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,16 +1,23 @@
 import User from "../models/user.model.js";
+import jwt from "jsonwebtoken";
 
 const authController = async (req, res) => {
   const { username, email, password } = req.body;
 
-  const result = await User.create({
+  const user = await User.create({
     username,
     email,
     password,
   });
 
+  const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+
+  const cookie = res.cookie("token", token);
+  console.log(cookie);
+
   res.status(201).json({
     message: "User register successfuly",
+    user,
   });
 };
 
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import connectDb from "./db/db.js";
 import authRoutes from "./routes/authRoutes.js";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 const app = express();
@@ -10,8 +11,13 @@ connectDb();
 
 const PORT = process.env.PORT || 3000;
 app.use(express.json());
-
+app.use(cookieParser());
 app.use("/api", authRoutes);
+app.get("/user", (req, res) =>{
+   const cookie = req.cookies;
+   console.log(cookie);
+   res.send(cookie);
+})
 
 app.listen(PORT, () => {
   console.log(`Server is running at ${PORT}`);
